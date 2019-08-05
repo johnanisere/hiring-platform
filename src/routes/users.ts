06f0827c813ff4b067baa353f32343e3ac9369e4
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-05 14:30:03 +0100

    imports authMiddleware and includes it in routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { authMiddleware } from '../middleware/auth';
 // import {} from '../controllers/authentication';
 
 const router = Router();
 
-router.post('/', function(_req, res, _next) {
+router.post('/', authMiddleware, function(_req, res, _next) {
   // const message = sampleController();
   // res.status(200).json({ message });
 });
