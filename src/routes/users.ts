c788c7ad5d2b2fe316f833c2328dd26000165099
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-05 13:36:41 +0100

    adds users route and post method for authentication

---
+++ src/routes/users.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+// import {} from '../controllers/authentication';
+
+const router = Router();
+
+router.post('/', function(_req, res, _next) {
+  // const message = sampleController();
+  // res.status(200).json({ message });
+});
+
+export default router;
