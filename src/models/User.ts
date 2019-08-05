7e81cce7f994e5f5a1244ab9273694d4c742f527
seun jay <seunjay92@gmail.com>
2019-08-05 16:34:51 +0100

    create User schema

---
+++ src/models/User.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from 'mongoose';
+
+export interface IUser extends mongoose.Document {
+  email: string;
+  password: string;
+  role: string;
+  name: string;
+  profilePhoto: string;
+  skills?: [string];
+  publications: string;
+  cv?: string;
+  bio?: string;
+  notifications: string;
+  contactPerson: string;
+  phone: string;
+  companyURL?: string;
+  address: string;
+}
+
+const UserSchema: Schema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String },
+  name: { type: String, required: true },
+  profilePhoto: { type: String },
+  skills: { type: mongoose.Schema.Types.ObjectId, ref: 'Skill' },
+  publications: { type: mongoose.Schema.Types.ObjectId, ref: 'Publication' },
+  cv: { type: String },
+  bio: { type: String },
+  notifications: { type: String },
+  contactPerson: { type: String },
+  phone: { type: String, required: true },
+  companyURL: { type: String },
+  address: { type: String, required: true },
+});
+
+export default mongoose.model<IUser>('User', UserSchema);
