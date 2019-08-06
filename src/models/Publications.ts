55e1eca9e245d6ad826bfa0b779b70b5f5ed0cc6
seun jay <seunjay92@gmail.com>
2019-08-06 12:29:17 +0100

    correct typo for the schema name

--- src/models/Publications.ts
+++ src/models/Publications.ts
@@ -4,12 +4,9 @@ export interface IPublications extends mongoose.Document {
   publication: String;
 }
 
-const NotificationSchema: Schema = new Schema({
+const PublicationSchema: Schema = new Schema({
   publication: { type: String },
   user: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
-export default mongoose.model<IPublications>(
-  'Publications',
-  NotificationSchema,
-);
+export default mongoose.model<IPublications>('Publications', PublicationSchema);
