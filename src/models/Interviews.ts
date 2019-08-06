1fdc09006702ebb7f93e3e04624d2367aa3a1a6c
seun jay <seunjay92@gmail.com>
2019-08-06 13:04:03 +0100

    create Interviews schema

---
+++ src/models/Interviews.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from 'mongoose';
+
+export interface IInterviews extends mongoose.Document {
+  hiringPartner: String;
+  decaDev: String;
+  location: String;
+  time: String;
+  description: String;
+  accepted: Boolean;
+}
+
+const InterviewsSchema: Schema = new Schema({
+  hiringPartner: { type: String, required: true },
+  decaDev: { type: String, required: true },
+  location: { type: String, required: true },
+  time: { type: String, required: true },
+  description: { type: String, required: true },
+  accepted: { type: Boolean },
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+});
+
+export default mongoose.model<IInterviews>('Interviews', InterviewsSchema);
