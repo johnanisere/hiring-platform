dbdccbf4af2bae96e1ba615e873f095f996b6424
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-06 12:40:50 +0100

    modifies post router for creating user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,18 +1,10 @@
 import { Router } from 'express';
-import { authMiddleware } from '../middleware/auth';
-import { createUser } from '../controllers/authentication';
+// import { authMiddleware } from '../middleware/auth';
+import createUser from '../controllers/authentication';
 
 const router = Router();
 
-router.post('/', async function(req, res, _next) {
-  const operation = await createUser(req.body);
-
-  res.json(operation);
-});
-
-router.get('/profile', authMiddleware, async function(_req, res, _next) {
-  res.json({ message: 'Hurray, you made it!' });
-});
+router.post('/', createUser);
 
 router.post('/', async function(_req, res, _next) {
   res.send('update password');
