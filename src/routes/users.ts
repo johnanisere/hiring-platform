65575028c937ba14170cbdea21410e58e9dedd34
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-06 11:14:30 +0100

    adds request to users route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,21 @@
 import { Router } from 'express';
 import { authMiddleware } from '../middleware/auth';
-// import {} from '../controllers/authentication';
+import { createUser } from '../controllers/authentication';
 
 const router = Router();
 
-router.post('/', authMiddleware, function(_req, res, _next) {
-  // const message = sampleController();
-  // res.status(200).json({ message });
+router.post('/', async function(req, res, _next) {
+  const operation = await createUser(req.body);
+
+  res.json(operation);
+});
+
+router.get('/profile', authMiddleware, async function(_req, res, _next) {
+  res.json({ message: 'Hurray, you made it!' });
+});
+
+router.post('/', async function(_req, res, _next) {
+  res.send('update password');
 });
 
 export default router;
