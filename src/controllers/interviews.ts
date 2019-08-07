6dd18ba031b5087d48862375717c4ec7735355d4
seun jay <seunjay92@gmail.com>
2019-08-07 10:20:32 +0100

    specify all interview fields expect its id

--- src/controllers/interviews.ts
+++ src/controllers/interviews.ts
@@ -4,9 +4,16 @@ export const scheduleInterview = async (req: any, res: any) => {
   const interview = new Interviews(req.body);
   try {
     const savedInterview = await interview.save();
-    savedInterview._id = undefined;
-    savedInterview.__v = undefined;
-    return res.status(201).json({ interview: savedInterview });
+    return res.status(201).json({
+      interview: {
+        hiringPartner: savedInterview.hiringPartner,
+        decaDev: savedInterview.decaDev,
+        location: savedInterview.location,
+        time: savedInterview.time,
+        description: savedInterview.description,
+        accepted: savedInterview.accepted,
+      },
+    });
   } catch (error) {
     return res.status(404).json(`Error: ${error}`);
   }
