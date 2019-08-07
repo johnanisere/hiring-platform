8855a3da1a1ee47c4ccbe8939af609889a25e278
seun jay <seunjay92@gmail.com>
2019-08-07 16:27:38 +0100

    delete file

--- src/routes/interview.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { scheduleInterview } from '../controllers/interviews';
-
-const router = Router();
-
-router.post('/invite/:userId', scheduleInterview);
-
-export default router;
