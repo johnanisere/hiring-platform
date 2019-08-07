e14804727db71a18aafa4b97b2ddfc30e46dad1a
seun jay <seunjay92@gmail.com>
2019-08-07 16:43:38 +0100

    remove reference to User schema

--- src/models/Skills.ts
+++ src/models/Skills.ts
@@ -6,7 +6,6 @@ export interface ISkills extends mongoose.Document {
 
 const SkillSchema: Schema = new Schema({
   skill: { type: String },
-  user: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
 export default mongoose.model<ISkills>('Skills', SkillSchema);
