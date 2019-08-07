e9353d93c9294d5bff8e2a54de5a8838a78913f6
seun jay <seunjay92@gmail.com>
2019-08-07 14:04:35 +0100

    comment out lines of non-working code

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 // import { authMiddleware } from '../middleware/auth';
 import createUser from '../controllers/authentication';
 import updatePassword from '../controllers/passwordUpdate';
+//import { acceptInterview } from '../controllers/interviews';
 
 const router = Router();
 
 router.post('/', createUser);
 
 router.put('/passwordUpdate/', updatePassword);
+//router.get('/acceptInterview/:interviewId/:userId', acceptInterview);
 
 export default router;
