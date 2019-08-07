cdeb6791bc1a86ab2725d032f1269ac27c1d53fe
seun jay <seunjay92@gmail.com>
2019-08-07 14:01:45 +0100

    comment acceptInterview route handler

--- src/controllers/interviews.ts
+++ src/controllers/interviews.ts
@@ -1,6 +1,7 @@
 import Interviews from '../models/Interviews';
+import { Request, Response } from 'express';
 
-export const scheduleInterview = async (req: any, res: any) => {
+export const scheduleInterview = async (req: Request, res: Response) => {
   const interview = new Interviews(req.body);
   try {
     const savedInterview = await interview.save();
@@ -18,3 +19,18 @@ export const scheduleInterview = async (req: any, res: any) => {
     return res.status(404).json(`Error: ${error}`);
   }
 };
+
+// export const acceptInterview = async (req: Request, res: Response) => {
+//   try {
+//     const interview = await Interviews.findById(req.params.id);
+//     if (!interview) {
+//       return res.send('No interview found');
+//     } else {
+//       interview.accepted = true;
+//       const newInterviewStatus = await interview.save();
+//       return res.status(200).json({ newInterviewStatus });
+//     }
+//   } catch (error) {
+//     return res.status(404).json(`Error: ${error}`);
+//   }
+// };
