cfd6ff848c1a6b7446b44e9de15da15eeec14463
seun jay <seunjay92@gmail.com>
2019-08-07 16:25:27 +0100

    define controller for scheduling interview

---
+++ src/controllers/interviews.ts
@@ -0,0 +1,21 @@
+import Interviews from '../models/Interviews';
+import { Request, Response } from 'express';
+
+export const scheduleInterview = async (req: Request, res: Response) => {
+  const interview = new Interviews(req.body);
+  try {
+    const savedInterview = await interview.save();
+    return res.status(201).json({
+      interview: {
+        hiringPartner: savedInterview.hiringPartner,
+        decaDev: savedInterview.decaDev,
+        location: savedInterview.location,
+        time: savedInterview.time,
+        description: savedInterview.description,
+        accepted: savedInterview.accepted,
+      },
+    });
+  } catch (error) {
+    return res.status(404).json(`Error: ${error}`);
+  }
+};
