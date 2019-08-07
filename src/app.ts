166266141be804cd657c1ad64c4d6f6b54835c11
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-07 11:44:26 +0100

    modifies user route

--- src/app.ts
+++ src/app.ts
@@ -52,8 +52,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', apiRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 app.use(
   '/graphql',
