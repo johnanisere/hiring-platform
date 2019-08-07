13b53eb5c2ee82434469e7d861f88880e2ef124f
seun jay <seunjay92@gmail.com>
2019-08-07 17:46:32 +0100

    add interview id

--- src/controllers/interviews.ts
+++ src/controllers/interviews.ts
@@ -6,14 +6,13 @@ export const scheduleInterview = async (req: Request, res: Response) => {
   try {
     const savedInterview = await interview.save();
     return res.status(201).json({
-      interview: {
-        hiringPartner: savedInterview.hiringPartner,
-        decaDev: savedInterview.decaDev,
-        location: savedInterview.location,
-        time: savedInterview.time,
-        description: savedInterview.description,
-        accepted: savedInterview.accepted,
-      },
+      hiringPartner: savedInterview.hiringPartner,
+      decaDev: savedInterview.decaDev,
+      location: savedInterview.location,
+      time: savedInterview.time,
+      description: savedInterview.description,
+      accepted: savedInterview.accepted,
+      id: savedInterview._id,
     });
   } catch (error) {
     return res.status(404).json(`Error: ${error}`);
