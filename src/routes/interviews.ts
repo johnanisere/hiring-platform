b283b1f6a552f718a1b444f8c6f0ed468d7bf3b0
seun jay <seunjay92@gmail.com>
2019-08-08 11:01:52 +0100

    rename api route and add route handler

--- src/routes/interviews.ts
+++ src/routes/interviews.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { scheduleInterview } from '../controllers/interviews';
+import { scheduleInterview, acceptInterview } from '../controllers/interviews';
 
 const router = Router();
 
 router.post('/invite', scheduleInterview);
-router.put('/invite/:inter');
+router.put('/invite/:interviewId', acceptInterview);
 
 export default router;
