eea464643c677df7b11ac51d6d5d05d7d6622873
seun jay <seunjay92@gmail.com>
2019-08-08 13:54:21 +0100

    import errors from celebrate

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { errors } from 'celebrate';
 import dotenv from 'dotenv';
 import createError from 'http-errors';
 import express from 'express';
@@ -56,6 +57,7 @@ app.use(compression());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(errors());
 
 app.use('/api', apiRouter);
 app.use('/api/v1/users', usersRouter);
