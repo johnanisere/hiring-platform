4996c2cb7f47f4d36fc1e05c858b9603dacb354e
seun jay <seunjay92@gmail.com>
2019-08-08 13:55:21 +0100

    create validation for interview invite

---
+++ src/validator/interviewValidation.ts
@@ -0,0 +1,22 @@
+import joi from '@hapi/joi';
+
+export const interviewValidation = (data: any) => {
+  const schema = {
+    hiringPartner: joi
+      .string()
+      .required()
+      .email(),
+    decaDev: joi
+      .string()
+      .required()
+      .email(),
+    location: joi.string().required(),
+    time: joi.string().required(),
+    description: joi
+      .string()
+      .required()
+      .max(100),
+  };
+
+  return joi.validate(data, schema);
+};
