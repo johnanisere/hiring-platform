99fb796ffd1b444f3739d1ee891d5fe20bd34431
seun jay <seunjay92@gmail.com>
2019-08-08 06:13:49 +0100

    modify schedduleInterview controller implementation

--- src/controllers/interviews.ts
+++ src/controllers/interviews.ts
@@ -1,10 +1,28 @@
 import Interviews from '../models/Interviews';
 import { Request, Response } from 'express';
+import User from '../models/User';
 
 export const scheduleInterview = async (req: Request, res: Response) => {
   const interview = new Interviews(req.body);
   try {
+    const hiringPartner = await User.findOne({ email: req.body.hiringPartner });
+    hiringPartner && hiringPartner.interviews.push(interview._id);
+    hiringPartner && (await hiringPartner.save());
+  } catch (err) {
+    res.status(400).send(err);
+  }
+
+  try {
+    const decaDev = await User.findOne({ email: req.body.decaDev });
+    decaDev && decaDev.interviews.push(interview._id);
+    decaDev && (await decaDev.save());
+  } catch (error) {
+    res.status(400).send(error);
+  }
+
+  try {
     const savedInterview = await interview.save();
+
     return res.status(201).json({
       hiringPartner: savedInterview.hiringPartner,
       decaDev: savedInterview.decaDev,
