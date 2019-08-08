6f362e712d5a0dbf7e1df0a48a5e63c46583f73a
seun jay <seunjay92@gmail.com>
2019-08-08 11:02:04 +0100

    add role

--- __tests__/routes.ts
+++ __tests__/routes.ts
@@ -27,6 +27,7 @@ describe('User Route', () => {
         email: 'careers@flutterwave.com',
         phone: '08074382109',
         password: 'password',
+        role: 'Hiring Partner',
       })
       .expect(res => {
         expect(res.body).toEqual(
@@ -40,6 +41,7 @@ describe('User Route', () => {
             createdAt: expect.any(String),
             updatedAt: expect.any(String),
             id: expect.any(String),
+            role: expect.any(String),
           }),
         );
       });
@@ -65,9 +67,29 @@ describe('User Route', () => {
         );
       });
   });
+
+  test('lists all decadevs', () => {
+    return request(app)
+      .get('/api/v1/users/decadevs')
+      .expect(res => {
+        expect(res).toEqual(
+          expect.objectContaining({
+            email: 'janedoe@example.com',
+            name: 'Jane Mary',
+            phone: '08067890545',
+            password: 'mysecret',
+            role: 'dev',
+            profilePhoto: 'http://gravatar.com/profile_photo-1',
+            cv: 'resume-1',
+            bio: 'A very good dev',
+          }),
+        );
+      });
+  });
+
   test('schedule interview', () => {
     return request(app)
-      .post('/api/v1/interview/invite/:userId')
+      .post('/api/v1/interview/invite/')
       .send({
         hiringPartner: 'terragon@gmail.com',
         decaDev: 'esther@gmail.com',
@@ -82,18 +104,6 @@ describe('User Route', () => {
         expect(Object.keys(res.body)).toContain('location');
         expect(Object.keys(res.body)).toContain('time');
         expect(Object.keys(res.body)).toContain('description');
-      });
-  });
-
-  test('lists all decadevs', () => {
-    return request(app)
-      .get('/api/v1/users/decadevs')
-      .expect(res => {
-        expect(res.body).toEqual(
-          expect.objectContaining({
-            message: 'No Decadevs found!',
-          }),
-        );
       });
   });
 });
