dcc08a729a18009b4e2eb87a5d47e2b022f3f292
seun jay <seunjay92@gmail.com>
2019-08-08 06:16:02 +0100

    rename route for scheduling interview

--- src/routes/interviews.ts
+++ src/routes/interviews.ts
@@ -3,6 +3,7 @@ import { scheduleInterview } from '../controllers/interviews';
 
 const router = Router();
 
-router.post('/invite/:userId', scheduleInterview);
+router.post('/invite', scheduleInterview);
+router.put('/invite/:inter');
 
 export default router;
