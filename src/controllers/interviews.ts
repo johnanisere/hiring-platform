132e7594658b0afaa187dc6f4fefb80c3db01705
seun jay <seunjay92@gmail.com>
2019-08-08 13:54:51 +0100

    add validation for interview

--- src/controllers/interviews.ts
+++ src/controllers/interviews.ts
@@ -1,8 +1,12 @@
 import Interviews from '../models/Interviews';
 import { Request, Response } from 'express';
 import User from '../models/User';
+import { interviewValidation } from '../validator/interviewValidation';
+//import { celebrate, Joi } from 'celebrate';
 
 export const scheduleInterview = async (req: Request, res: Response) => {
+  const { error } = interviewValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
   const interview = new Interviews(req.body);
   try {
     const hiringPartner = await User.findOne({ email: req.body.hiringPartner });
@@ -49,3 +53,15 @@ export const acceptInterview = async (req: Request, res: Response) => {
     return res.status(404).json(`Error: ${error}`);
   }
 };
+
+// export const validator = celebrate({
+//   body: Joi.object().keys({
+//     hiringPartner: Joi.string().required().email(),
+//     decaDev: Joi.string().required().email(),
+//     location: Joi.string().required(),
+//     time: Joi.string().required(),
+//     description: Joi.string().required()
+//   }),
+// });
+
+// export default validator
