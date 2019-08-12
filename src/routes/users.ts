67f00d078f40914c8479351da820ab899e1040ec
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-12 16:09:20 +0100

    created the route for the change password

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import createUser from '../controllers/authentication';
 import updatePassword from '../controllers/passwordUpdate';
 import getAllDecadevs from '../controllers/decadevs';
-
+import changePassword from '../controllers/changePassword';
 const router = Router();
 
 router
   .get('/decadevs', getAllDecadevs)
   .post('/hiring-partner/invite', createUser)
-  .put('/update-password/', updatePassword);
+  .put('/update-password/', updatePassword)
+  .put('/change-password', changePassword);
 
 export default router;
