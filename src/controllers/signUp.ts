2ce112cb9b6f1cd9464b730d94fcd80b6e4dc9a6
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-08-13 15:30:20 +0100

    Add sign up feature

---
+++ src/controllers/signUp.ts
@@ -0,0 +1,24 @@
+import User from '../models/User';
+import { Response, Request, NextFunction } from 'express';
+
+export default async function signUp(
+  req: Request,
+  res: Response,
+  _next: NextFunction,
+) {
+  try {
+    const user = await new User(req.body);
+    const newUser = await user.save();
+    return res.status(200).send({
+      output: 'Sign Up successful.',
+      newUser,
+    });
+  } catch (error) {
+    const { message } = error;
+    console.log(error.message);
+    return res.status(400).json({
+      output: 'Sign Up not successful!!!',
+      message,
+    });
+  }
+}
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,11 +3,13 @@ import inviteHiringPartner from '../controllers/authentication';
 import updatePassword from '../controllers/passwordUpdate';
 import getAllDecadevs from '../controllers/decadevs';
 import userLogin from '../controllers/userLogin';
+import signUp from '../controllers/signUp';
 
 const router = Router();
 
 router
   .get('/decadevs', getAllDecadevs)
+  .post('/signup', signUp)
   .post('/hiring-partner/invite', inviteHiringPartner)
   .put('/update-password/', updatePassword)
   .post('/login', userLogin);
