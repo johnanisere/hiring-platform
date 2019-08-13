540056b5d675a61da8347053fee335d7f1bf2fb8
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-08-13 15:30:20 +0100

    Add sign up feature

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
-import inviteHiringPartner from '../controllers/authentication';
+import inviteHiringPartner from '../controllers/inviteHiringPartner';
 import updatePassword from '../controllers/passwordUpdate';
 import getAllDecadevs from '../controllers/decadevs';
+import signUp from '../controllers/signUp';
 
 const router = Router();
 
 router
   .get('/decadevs', getAllDecadevs)
+  .post('/signup', signUp)
   .post('/hiring-partner/invite', inviteHiringPartner)
   .put('/update-password/', updatePassword);
 
