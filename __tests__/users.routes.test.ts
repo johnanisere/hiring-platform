2f1dbe8e8b82e1cc4eba09cbc551d8bf11f87992
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-16 17:38:19 +0100

    separates test for different routes into different files

--- __tests__/users.routes.test.ts
+++ __tests__/users.routes.test.ts
@@ -1,5 +1,4 @@
 import request from 'supertest';
-import Interviews from '../src/models/Interviews';
 import app from '../src/app';
 // import auth from '../src/middleware/auth';
 
@@ -8,71 +7,6 @@ const { connectMongoDB, disconnectMongoDB } = require('../testSetup/mongodb');
 beforeAll(async () => await connectMongoDB());
 
 afterAll(() => disconnectMongoDB());
-
-describe('interview route', () => {
-  test('schedule interview', () => {
-    return request(app)
-      .post('/api/v1/interview/invite/')
-      .send({
-        hiringPartner: 'terragon@gmail.com',
-        decaDev: 'esther@gmail.com',
-        location: 'Victoria Island',
-        time: '10am',
-        description:
-          'This is to inform you that you have been shortlisted for an interview',
-        profilePhoto:
-          'https://res.cloudinary.com/demo/image/upload/w_150,h_150,c_thumb,g_fac...',
-      })
-      .expect(res => {
-        expect(Object.keys(res.body)).toContain('hiringPartner');
-        expect(Object.keys(res.body)).toContain('decaDev');
-        expect(Object.keys(res.body)).toContain('location');
-        expect(Object.keys(res.body)).toContain('time');
-        expect(Object.keys(res.body)).toContain('description');
-      });
-  });
-
-  test('accept interview', async () => {
-    let id;
-    const interview = new Interviews({
-      hiringPartner: 'google@gmail.com',
-      decaDev: 'chukky@gmail.com',
-      location: 'Ikeja',
-      time: '9am',
-      description:
-        'This is to inform you that you have been shortlisted for an interview',
-      profilePhoto:
-        'https://res.cloudinary.com/demo/image/upload/w_150,h_150,c_thumb,g_fac...',
-    });
-
-    const savedInterview = await interview.save();
-
-    id = savedInterview._id;
-
-    return request(app)
-      .put(`/api/v1/interview/invite/${id}`)
-      .send({ accepted: true })
-      .expect(res => {
-        expect(Object.keys(res.body.interview)).toContain('accepted');
-        expect(Object.keys(res.body.interview)).toContain('hiringPartner');
-      });
-  });
-
-  test('invite devs', async () => {
-    return request(app)
-      .post('/api/v1/invite/devs')
-      .send({
-        squadNo: 'Squad 1',
-      })
-      .expect(res => {
-        expect(res.body).toEqual(
-          expect.objectContaining({
-            message: `Your invites have been sent to Squad 1`,
-          }),
-        );
-      });
-  });
-});
 
 describe('User Route', () => {
   test('Has a /api endpoint', () => {
