2047c6b689a98729fb5eb395133b86a20ba09ec4
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-19 21:09:20 +0100

    modifies updatePassword routes

--- src/controllers/passwordUpdate.ts
+++ src/controllers/passwordUpdate.ts
@@ -7,15 +7,16 @@ export default async function updatePassword(
   _next: NextFunction,
 ) {
   try {
-    const user = await User.findOne({ email: req.body.email });
+    const { email } = req.body.user;
+    const user = await User.findOne({ email });
 
     if (!user) {
-      return res.send('User not found!!!');
+      res.send('User not found!!!');
     } else {
       user.password = req.body.newPassword;
       const updated = await user.save();
 
-      return res.status(200).send({
+      res.status(200).send({
         message: 'Password updated',
         updated: {
           name: updated.name,
@@ -25,7 +26,7 @@ export default async function updatePassword(
       });
     }
   } catch (err) {
-    return res.status(400).json({
+    res.status(400).send({
       message: 'Password update failed!!!',
       error: err.message,
     });
