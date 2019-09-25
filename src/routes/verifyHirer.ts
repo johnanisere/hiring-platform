cd0d3f1c78e6dbf0e872d342c0f761c1a90865cf
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-25 15:41:40 +0100

    adds verify hirer route

--- src/routes/verifyHirer.ts
+++ src/routes/verifyHirer.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getAllUnverified } from '../controllers/hiringPartners';
+import { getAllUnverified } from '../controllers/getAllUnverified';
+import { verifyHirer } from '../controllers/verifyHiringPartner';
 
 const router = Router();
 
-router.get('/unverified', getAllUnverified);
+router.get('/unverified', getAllUnverified).put('/verifyhirer', verifyHirer);
 
 export default router;
