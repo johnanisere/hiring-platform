4080d24191ea78b59cb7504d8370903766c7a125
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-26 12:23:51 +0100

    modifies code

--- src/controllers/getAllUnverified.ts
+++ src/controllers/getAllUnverified.ts
@@ -6,9 +6,12 @@ export async function getAllUnverified(_req: Request, res: Response) {
     const unverifiedHirers = await HiringPartner.find({ isVerified: false });
     if (unverifiedHirers[0]) {
       res.status(200).send(unverifiedHirers);
+      return;
     }
     res.status(204).send({ message: 'No unverified Hiring Partner' });
+    return;
   } catch (err) {
     res.status(400).send(err.message);
+    return;
   }
 }
