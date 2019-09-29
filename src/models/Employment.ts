c3401a22baaff03c3ce3ce03e991a32e2b08e357
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-29 20:39:00 +0100

    adds employment schema

---
+++ src/models/Employment.ts
@@ -0,0 +1,15 @@
+import { Schema } from 'mongoose';
+
+export interface IEmployment {
+  title: String;
+  duration: String;
+  location: String;
+  achievements: Array<String>;
+}
+
+export const EmploymentSchema: Schema = new Schema({
+  title: { type: String },
+  duration: { type: String },
+  location: { type: String },
+  achievements: { type: [String] },
+});
