6d43708d1f668686fac05df0f1733abb0941f864
EstherSeyi <sheyiogundijo@gmail.com>
2019-11-13 17:05:13 +0100

    modifies backend controllers to suit new read from csv feature

--- src/controllers/inviteDevs.ts
+++ src/controllers/inviteDevs.ts
@@ -3,30 +3,29 @@ import devsMailInvite from '../utils/devinvite';
 import jwt from 'jsonwebtoken';
 import { PRIVATE_KEY } from '../config';
 
-const sendInviteEmail = () => {
-  const invites = [{ name: 'seun jay', email: 'seunjay92@gmail.com' }].map(
-    async ({ name, email }) => {
-      const token = jwt.sign(
-        {
-          email: email,
-        },
-        PRIVATE_KEY,
-        {
-          expiresIn: '1h',
-        },
-      );
-      return await devsMailInvite(token, name, email);
-    },
-  );
+const sendInviteEmail = (emails: []) => {
+  const invites = emails.map(async (email: string) => {
+    let array = email.split(',');
+    const token = jwt.sign(
+      {
+        email: array[1],
+      },
+      PRIVATE_KEY,
+      {
+        expiresIn: '1h',
+      },
+    );
+    await devsMailInvite(token, array[0], array[1]);
+  });
 
   Promise.all(invites);
 };
 export const inviteDevs = async (req: Request, res: Response) => {
   try {
-    const { squadNo } = req.body;
-    sendInviteEmail();
+    const emails = req.body;
+    sendInviteEmail(emails);
     return res.json({
-      message: `Your invites have been sent to ${squadNo}`,
+      message: `Your invites have been sent`,
     });
   } catch (error) {
     return res.status(404).json(`Error: ${error}`);
