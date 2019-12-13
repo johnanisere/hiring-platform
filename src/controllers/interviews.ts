9ca46c3c3c4333b55b2bb23b5af729bd46e77d90
EstherSeyi <sheyiogundijo@gmail.com>
2019-12-13 15:19:46 +0100

    modifies interview controller to suit new feature of hirer being notified of reschedules

--- src/controllers/interviews.ts
+++ src/controllers/interviews.ts
@@ -1,5 +1,6 @@
 import Interviews from '../models/Interviews';
 import { Request, Response } from 'express';
+import HiringPartner from '../models/HiringPartner';
 import User from '../models/User';
 import { interviewValidation } from '../validator/interviewValidation';
 import interviewInvitationMail from '../utils/interviewInvitation';
@@ -7,54 +8,47 @@ export const scheduleInterview = async (req: Request, res: Response) => {
   try {
     const { error, value } = interviewValidation(req.body);
     if (error) return res.status(400).send('Invalid Fields');
+
     const interview = new Interviews(req.body);
+    const savedInterview = await interview.save();
 
-    try {
-      const hiringPartner = await User.findOne({
-        email: req.body.hiringPartner,
-      });
+    const hiringPartner = await HiringPartner.findOne({
+      email: value.hiringPartner,
+    });
 
-      hiringPartner && hiringPartner.interviews.push(interview._id);
-      hiringPartner && (await hiringPartner.save());
-    } catch (err) {
-      res.status(400).send(err);
+    if (!hiringPartner) {
+      res.status(404).json({ message: 'partner not found' });
+      return;
     }
 
-    try {
-      const decaDev = await User.findOne({ email: req.body.decaDev });
+    hiringPartner.interviews.push(interview._id);
+    hiringPartner.markModified('interviews');
+    await hiringPartner.save();
 
-      if (decaDev) {
-        decaDev.interviews.push(interview._id);
-        await decaDev.save();
-        interviewInvitationMail(req, decaDev, interview._id);
-      }
-    } catch (error) {
-      res.status(400).send(error);
-    }
+    const decaDev = await User.findOne({ email: req.body.decaDev });
 
-    try {
-      const savedInterview = await interview.save();
+    if (!decaDev) return;
+    decaDev.interviews.push(interview._id);
+    await decaDev.save();
+    interviewInvitationMail(req, decaDev, interview._id);
 
-      return res.status(201).json({
-        interviewData: {
-          hiringPartner: savedInterview.hiringPartner,
-          decaDev: savedInterview.decaDev,
-          location: savedInterview.location,
-          startTime: savedInterview.startTime,
-          endTime: savedInterview.endTime,
-          startDate: savedInterview.startDate,
-          endDate: savedInterview.endDate,
-          description: savedInterview.description,
-          id: savedInterview._id,
-          scheduled: savedInterview.startDate,
-        },
-        message: "Interview has been sent to Decadev's email",
-      });
-    } catch (error) {
-      return res.status(404).json(`Error: ${error}`);
-    }
+    return res.status(200).json({
+      interviewData: {
+        hiringPartner: savedInterview.hiringPartner,
+        decaDev: savedInterview.decaDev,
+        location: savedInterview.location,
+        startTime: savedInterview.startTime,
+        endTime: savedInterview.endTime,
+        startDate: savedInterview.startDate,
+        endDate: savedInterview.endDate,
+        description: savedInterview.description,
+        id: savedInterview._id,
+        scheduled: savedInterview.startDate,
+      },
+      message: "Interview has been sent to Decadev's email",
+    });
   } catch (err) {
-    return console.log({ error: err.message });
+    return res.send({ actual: err.message, message: 'Error, Process failed!' });
   }
 };
 
